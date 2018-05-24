0861bde5ab41799337f644a228995ed76fa72ee3
“swgoodman” <spencerwgoodman@icloud.com>
2018-05-24 14:41:36 -0700

    Added author scrape.

--- lib/pitchfork_latest/review.rb
+++ lib/pitchfork_latest/review.rb
@@ -1,6 +1,6 @@
 
 class PitchforkLatest::Review
-  attr_accessor :url, :album, :artist, :genre, :score, :first_paragraph
+  attr_accessor :url, :album, :artist, :genre, :score, :first_paragraph, :author
   @@all = []
 
   def initialize
@@ -30,9 +30,10 @@ class PitchforkLatest::Review
   def scrape_info
 
     doc = Nokogiri::HTML(open(self.url))
-    # binding.pry
+    binding.pry
     self.score = doc.search('span.score').text
     self.first_paragraph = doc.xpath('//p').text.gsub!(/[^A-Za-z ."  ']/,'')
+    self.author = doc.search('a.authors-detail__display-name').text
 
 
   end
