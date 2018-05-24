8cb9534584f53259e83873d22597844a8628a005
“swgoodman” <spencerwgoodman@icloud.com>
2018-05-24 03:17:54 -0700

    First scraper done.

--- lib/pitchfork_latest/review.rb
+++ lib/pitchfork_latest/review.rb
@@ -11,12 +11,19 @@ class PitchforkLatest::Review
 
     doc = Nokogiri::HTML(open("https://pitchfork.com/"))
 
-    doc.search("div.album-review-hero").each do |review|
+    review_blocks = []
+    doc.search("div.album-review-hero")[1..4].each do |review|
+      review_blocks << review
+    end
+
+    review_blocks.each.with_index do |review, i|
       review = self.new
       review.url = doc.search("div.album-details a").attribute("href").text
-      review.album = doc.search("ul.artist-list").text
-      review.artist = doc.search("h2.title").text
-      review.genre = doc.search("ul.genre-list").text
+      review.artist = doc.search("ul.artist-list")[i].text.gsub(/[^a-zA-Z 0-9]/, "").gsub(/\s+/, ' ')
+
+      review.album = doc.search("h2.title")[i+2].text.gsub(/[^a-zA-Z 0-9]/, "").gsub(/\s+/, ' ')
+      review.genre = doc.search("ul.genre-list")[i].text
     end
+    @@all
   end
 end
